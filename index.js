cc5e81c25cb5ca219fd6bca9cf3a0c16d7b75871
aliceguegan <85225686+aliceguegan@users.noreply.github.com>
2022-03-08 11:43:50 +0100

    Update

--- index.js
+++ index.js
@@ -11,3 +11,29 @@
             document.getElementById('projets').style.opacity = 1;
         };
     });
+
+    document.querySelectorAll('.project-item').forEach(item => {
+        item.addEventListener("click", function(e){
+            e.preventDefault();
+            document.getElementById(item.id + '-details').classList.remove('opacity');
+            void document.getElementById(item.id + '-details').offsetWidth;
+            document.getElementById(item.id + '-details').classList.add('opacity');
+            document.getElementById(item.id + '-details').style.opacity = '1';
+            document.getElementById(item.id + '-details').style.display = 'block';
+            window.location.href = "#titre-projets";
+            document.querySelectorAll('.details-container:not(#' + item.id + '-details)').forEach(con => {
+                con.style.display = 'none';
+                con.style.opacity = '0';
+                con.classList.remove('opacity');
+            })
+        })
+    })
+
+    document.querySelectorAll('.details-container').forEach(item => {
+        item.querySelector('.fermer-projet').addEventListener('click', function(e){
+            e.preventDefault()
+            item.style.display = 'none';
+            item.style.opacity = '0';
+            item.classList.remove('opacity');
+        })
+    })
